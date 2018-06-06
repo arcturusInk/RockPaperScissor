9cf3782a91ee7aaad7c964d3dda9aa97f5a2d6ae
arcturusInk <arcturusInk@users.noreply.github.com>
2018-06-05 21:00:56 -0400

    Simpler, cleaner code

--- Main.java
+++ Main.java
@@ -2,21 +2,28 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
-    //an array of possible options
-    private final String[] choices = {"Rock", "Paper", "Scissor"};
+public class Game {
+
+    //Game Introduction
+    static void displayGameIntroduction(){
+        System.out.println("Lets play a Rock, Paper, or Scissor Game!");
+        System.out.println("Please enter either 'Rock', 'Paper', or 'Scissor'!");
+        System.out.println("Or type 'Exit' to close the game.");
+    }
 
     //pick a random choice from the array
-    //return the random choice
-    public String pickAnOption(){
+    //@param - takes in a array of strings that contains the computer's options
+    //@return -  the random choice
+    static String pickAnOption(String[] choices){
         int aPick = (int) (Math.random()*3);
         String computerPick = choices[aPick];
+        System.out.println("Computer's pick was " + computerPick +".");
         return  computerPick;
     }
 
     //ask user to enter their choice
-    //return that choice
-    public String askForInput(){
+    //@return -  user choice
+    static String askForInput(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your choice: ");
         String userInput = scan.next();
@@ -24,8 +31,8 @@ public class Main {
     }
 
     //function that determines who wins
-    //parameters are the users choice and computer's randomly pick
-    public void determineGamePlay(String userInput, String computerPick){
+    //@param - users choice and computer's randomly pick
+    static void determineGamePlay(String userInput, String computerPick){
         if (userInput.equals(computerPick)) {
             System.out.println("It's a draw!");
         } else if (userInput.equals("Rock") && computerPick.equals("Paper")) {
@@ -44,29 +51,16 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        String[] choices = {"Rock", "Paper", "Scissor"};
 
-        //Introduction to the game for the user
-        System.out.println("Lets play a Rock, Paper, or Scissor Game!");
-        System.out.println("Please enter either 'Rock', 'Paper', or 'Scissor'!");
-        System.out.println("Or type 'Exit' to close the game.");
-
-        Main anObject = new Main();
-        //ask for user's input
-        String userInput = anObject.askForInput();
-        //pick a random choice to play against
-        String computerPick = anObject.pickAnOption();
-
-        //if exit, then close the program
+        displayGameIntroduction();
+        String userInput = askForInput();
         if(userInput.equals("Exit")){
             System.exit(0);
-        }
-        //if user enters a valid choice
-        if(userInput.equals("Rock") || userInput.equals("Paper") || userInput.equals("Scissor")) {
-            System.out.println("Computer's pick was " + computerPick +".");
-            anObject.determineGamePlay(userInput, computerPick);
-        }
-        //if user does not enter a valid choice, ask them to enter a valid option
-        else{
+        }else if(userInput.equals("Rock") || userInput.equals("Paper") || userInput.equals("Scissor")){
+            String comInput = pickAnOption(choices);
+            determineGamePlay(userInput, comInput);
+        }else{
             System.out.println("Please enter either 'Rock', 'Paper', or 'Scissor' as a choice!");
         }
     }
